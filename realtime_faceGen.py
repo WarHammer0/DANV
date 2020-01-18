f64b3e73d0a7dd10b594385a6de643fa05d09bf3
Arthicha <zumoarthicha@gmail.com>
2020-01-18 11:16:44 +0700

    real-time with MATLAB MFCC

--- realtime_faceGen.py
+++ realtime_faceGen.py
@@ -6,7 +6,6 @@ from torch.utils.data import DataLoader
 import os
 import ntpath
 import cv2
-
 import torch
 import util.util as util
 import numpy as np
@@ -14,10 +13,14 @@ import pyaudio
 import wave
 from python_speech_features import mfcc
 import copy
-
-opt = BaseOptions().parse()
+import runmfcc
 import Test_Gen_Models.Test_Audio_Model as Gen_Model
 from Dataloader.Test_load_audio import Test_VideoFolder
+
+
+mat = runmfcc.initialize()
+opt = BaseOptions().parse()
+
 
 
 # global variable
@@ -32,33 +35,44 @@ def callback(in_data, frame_count, time_info, status):
     global RATE
     global mode
 
+
     if mode != 3:
         sig = np.fromstring(in_data, 'Int16')
     else:
-        sig = copy.deepcopy(wav[:CHUNK])+np.fromstring(in_data, 'Int16')
+        noi = 1000.0 * np.random.normal(0, 0.1, CHUNK)
+        noi = noi.astype(np.int16)
+        sig = copy.deepcopy(wav[:CHUNK])+noi
         wav = np.concatenate((wav[CHUNK:],wav[:CHUNK]))
+    out_data = copy.deepcopy(sig)
+
+
     sig = np.concatenate((streamedAudio, sig))
 
-    if sig.shape[0] > int(RATE*0.2):
-        streamedAudio = sig[int(-1.0*RATE*0.2):]
+    if sig.shape[0] > int(RATE*0.2*10):
+        streamedAudio = sig[int(-1.0*RATE*0.2*10):]
     else:
         streamedAudio = sig
+    print(len(streamedAudio))
+    mfcc_matlab = mat.runmfcc(sig.tolist())
+    mfcc_matlab = np.array(mfcc_matlab)
+    mfcc_matlab = np.transpose(mfcc_matlab)[-20:,1:]
 
-    mfcc_feat = zip(*mfcc(sig[:int(RATE*0.2)], RATE, winlen=0.025, winstep=0.0095, numcep=12,
+    '''mfcc_feat = mfcc(sig, RATE, winlen=0.025, winstep=0.01, numcep=13,
                           nfilt=13, nfft=2048, lowfreq=300, highfreq=3700, preemph=0.97, ceplifter=22,
-                          appendEnergy=True))
-    mfcc_feat = np.stack([np.array(i) for i in mfcc_feat])
+                          appendEnergy=True)
+    mfcc_feat = np.transpose(mfcc_feat)'''
+    mfcc_feat = mfcc_matlab
     audio_set = np.expand_dims(np.expand_dims(np.expand_dims(mfcc_feat, axis=0), axis=0), axis=0)
     mfcc_data = np.array(audio_set)
-    return (in_data, pyaudio.paContinue)
+    return (out_data, pyaudio.paContinue)
 
 
 
-mode = 3 # 1=ours, 2=benchmark, 3=combined
-CHUNK = 1600#4410
+mode = 1 # 1=ours, 2=benchmark, 3=combined
+CHUNK = 10000#4410
 WIDTH = 2
 CHANNELS = 1
-RATE = 16000#44100
+RATE = 44100
 t = time.time()
 mfcc_data = []
 p = pyaudio.PyAudio()
@@ -67,8 +81,8 @@ n = 1
 
 
 if mode == 3:
-    wf = wave.open('0572_0019_0003.wav', 'rb')
-    wav = wf.readframes(16000*4)
+    wf = wave.open('trump.wav', 'rb')
+    wav = wf.readframes(RATE*500)
     wav = wav[1::2]
     wav = np.fromstring(wav, 'Int16')
 
@@ -84,7 +98,7 @@ stream = p.open(format=pyaudio.paInt16,  # p.get_format_from_width(WIDTH),
 opt.nThreads = 1  # test code only supports nThreads = 1
 opt.batchSize = 1  # test code only supports batchSize = 1
 opt.sequence_length = 1
-test_nums = [3]
+test_nums = [7]
 model = Gen_Model.GenModel(opt)
 
 A_path = os.path.join(opt.test_A_path, 'test_sample' + str(test_nums[0]) + '.jpg')
@@ -123,6 +137,7 @@ while(1):
                 data['B_audio'] = dic['B_audio']
             else:
                 data['B_audio'] = mfcc_data_torch[0]
+            #print(data['B_audio'])
             model.set_test_input(data)
             model.test()
             visuals = model.get_current_visuals()
